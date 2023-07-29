383d71cfd392adf92e5c93574c0a9f541ecb9306
m00n-die <reginaldrock219@gmail.com>
2023-07-29 06:31:35 +0100

    pycodestyle corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,19 +15,23 @@ TODO = requests.get(f"{url}/todos", params={"userId": emp_id})
 emp_name = user.json().get("name")
 todo = TODO.json()
 
-#for item in todo:
-#if 'userId' in item:
-#all_tasks += 1
-#if 'completed' in item:
-#completed_tasks += 1
+"""
+for item in todo:
+    if 'userId' in item:
+        all_tasks += 1
+    if 'completed' in item:
+        completed_tasks += 1
+"""
 complete = []
 for item in todo:
     if item.get('completed') is True:
         complete.append(item.get('title'))
 
 print("Employee {} is done with tasks({}/{}):".format
-         (emp_name, len(complete), len(todo)))
-#print("Employee {} is done with tasks({}/{}):".format
-#(emp_name, completed_tasks, all_tasks))
+      (emp_name, len(complete), len(todo)))
+"""
+print("Employee {} is done with tasks({}/{}):".format
+        (emp_name, completed_tasks, all_tasks))
+"""
 
 [print("\t {}".format(elem)) for elem in complete]
