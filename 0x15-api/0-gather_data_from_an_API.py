a72e9592a3a900f750c0b746e309af1a3ca0b23c
m00n-die <reginaldrock219@gmail.com>
2023-07-29 06:37:41 +0100

    fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-"""
-module that gets data from an api
-"""
-
+"""Module that gets data from an api"""
 import requests
 import sys
 import json
 
 
-all_tasks = 0
-completed_tasks = 0
-emp_id = sys.argv[1]
+if __name__ == "__main__":
+    all_tasks = 0
+    completed_tasks = 0
+    emp_id = sys.argv[1]
 
-url = "https://jsonplaceholder.typicode.com"
-user = requests.get(f"{url}/users/{emp_id}")
-TODO = requests.get(f"{url}/todos", params={"userId": emp_id})
-emp_name = user.json().get("name")
-todo = TODO.json()
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{url}/users/{emp_id}")
+    TODO = requests.get(f"{url}/todos", params={"userId": emp_id})
+    emp_name = user.json().get("name")
+    todo = TODO.json()
 
-"""
-for item in todo:
-    if 'userId' in item:
-        all_tasks += 1
-    if 'completed' in item:
-        completed_tasks += 1
-"""
-complete = []
-for item in todo:
-    if item.get('completed') is True:
-        complete.append(item.get('title'))
+    """
+    for item in todo:
+        if 'userId' in item:
+            all_tasks += 1
+        if 'completed' in item:
+            completed_tasks += 1
+    """
 
-print("Employee {} is done with tasks({}/{}):".format
-      (emp_name, len(complete), len(todo)))
-"""
-print("Employee {} is done with tasks({}/{}):".format
-        (emp_name, completed_tasks, all_tasks))
-"""
+    complete = []
+    for item in todo:
+        if item.get('completed') is True:
+            complete.append(item.get('title'))
 
-[print("\t {}".format(elem)) for elem in complete]
+    print("Employee {} is done with tasks({}/{}):".format
+          (emp_name, len(complete), len(todo)))
+    """
+    print("Employee {} is done with tasks({}/{}):".format
+          (emp_name, completed_tasks, all_tasks))
+    """
+
+    [print("\t {}".format(elem)) for elem in complete]
