b10fba022e5c36dc15f27ba80ea0b6c835bf2ffb
m00n-die <reginaldrock219@gmail.com>
2023-07-29 06:22:41 +0100

    initialized directory

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""gets data from an api"""
+import requests
+import sys
+import json
+
+
+all_tasks = 0
+completed_tasks = 0
+emp_id = sys.argv[1]
+
+url = "https://jsonplaceholder.typicode.com"
+user = requests.get(f"{url}/users/{emp_id}")
+TODO = requests.get(f"{url}/todos", params={"userId": emp_id})
+emp_name = user.json().get("name")
+todo = TODO.json()
+
+#for item in todo:
+#if 'userId' in item:
+#all_tasks += 1
+#if 'completed' in item:
+#completed_tasks += 1
+complete = []
+for item in todo:
+    if item.get('completed') is True:
+        complete.append(item.get('title'))
+
+print("Employee {} is done with tasks({}/{}):".format
+         (emp_name, len(complete), len(todo)))
+#print("Employee {} is done with tasks({}/{}):".format
+#(emp_name, completed_tasks, all_tasks))
+
+for item in todo:
+    if 'completed' in item:
+        print("\t {}".format(item.get('title')))
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+Directory for all files and tasks under 0x15-api in the ALX SE Programme
